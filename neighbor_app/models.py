bb0e5c448333dcb55bbe72b421ab9f9832ea1303
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-10-31 15:13:16 +0300

    profile, models.py

--- neighbor_app/models.py
+++ neighbor_app/models.py
@@ -35,3 +35,26 @@ class NeighborHood(models.Model):
     def __str__(self):
         return self.name
 
+
+# Profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(max_length=150)
+    signup_confirmation = models.BooleanField(default=False)
+    bio =models.TextField(null=True)
+    profile_picture =CloudinaryField('image')
+    location =models.CharField(max_length=60,blank=True,null=True)
+    neighborhood = models.ForeignKey('NeighborHood', on_delete=SET_NULL,null=True, related_name='people', blank=True)
+    
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def update_profile_signal(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+    
