85c5b61054cf43dc00b3d5ac8ed6715176b7313f
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-10-31 15:16:28 +0300

    business, models.py

--- neighbor_app/models.py
+++ neighbor_app/models.py
@@ -57,4 +57,28 @@ def update_profile_signal(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
     instance.profile.save()
-    
+
+
+# Business
+class Business(models.Model):
+    name =models.CharField(max_length=60)
+    description = models.TextField()
+    image = CloudinaryField('image')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    neighborhood = models.ForeignKey(NeighborHood,on_delete=CASCADE,related_name='business')
+    user = models.ForeignKey(User,on_delete=CASCADE)
+    email = models.EmailField()
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    @classmethod
+    def search_businesses(cls, business):
+        return cls.objects.filter(name__icontains=business).all()
+
+    def __str__(self):
+        return self.name
