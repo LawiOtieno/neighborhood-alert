a03bea7223732873155534dbb6cbbf17c5a0f887
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-10-31 16:15:56 +0300

    login views

--- neighbor_app/views.py
+++ neighbor_app/views.py
@@ -58,3 +58,22 @@ def signup_view(request):
   else:
     form = SignUpForm()
   return render(request, 'registration/signup.html', {'form': form})
+
+# Login
+def login(request):
+  if request.method == 'POST':
+    form = AuthenticationForm(request=request, data=request.POST)
+    if form.is_valid():
+      username = form.cleaned_data.get('username')
+      password = form.cleaned_data.get('password')
+      user = authenticate(username=username, password=password)
+      if user is not None:
+        auth_login(request, user)
+        messages.info(request, f"You are now logged in as {username}")
+        return redirect('home')
+      else:
+        messages.error(request, "Invalid username or password.")
+    else:
+      messages.error(request, "Invalid username or password.")
+  form = AuthenticationForm()
+  return render(request = request,template_name = "registration/login.html",context={"form":form})
